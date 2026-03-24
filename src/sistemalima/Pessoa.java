e98d91c5cbe7ef2a97854267522188c383fd5a7a
Amanda Mareco Soares Lima LIMA <u04730809157@acad.pp.ifms.edu.br>
2026-03-24 07:59:02 -0400

    Primeiro envio do projeto para o git.

---
+++ src/sistemalima/Pessoa.java
@@ -0,0 +1,55 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package sistemalima;
+
+/**
+ *
+ * @author u04730809157
+ */
+public class Pessoa {
+    
+    private String nome;
+    private int idade;
+
+    public Pessoa() {
+    }
+
+    public Pessoa(String nome, int idade) {
+        this.nome = nome;
+        this.idade = idade;
+    }
+
+    public static void main(String[] args) {
+        
+    }
+
+    /**
+     * @return the nome
+     */
+    public String getNome() {
+        return nome;
+    }
+
+    /**
+     * @param nome the nome to set
+     */
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    /**
+     * @return the idade
+     */
+    public int getIdade() {
+        return idade;
+    }
+
+    /**
+     * @param idade the idade to set
+     */
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
+}
